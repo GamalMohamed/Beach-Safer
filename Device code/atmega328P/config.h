5328df04042b84bd2de9d0c616715c8ed55a236a
Amr Hossam <amrhossam902@gmail.com>
2018-02-28 00:09:46 +0200

    push button initialization added

--- Device code/atmega328P/config.h
+++ Device code/atmega328P/config.h
@@ -4,13 +4,18 @@
 #define MAX_RF_MSG_LENGTH 80
 #define RF_MSG_SENDING_PERIOD 30000
 
+//push button constants
+#define PUSH_INTERVAL 20000
+#define REL_INTERVAL 5000
+
 // Pin Layout configuration
-#define PUSH_BUTTON_PIN 7
+#define PUSH_BUTTON_PIN 2
 #define RF_RX_PIN 4
 #define RF_TX_PIN 8
 #define RF_PTT_PIN 5 
 //#define RELEASE_SERVO_PIN ??
 //#define PULL_SERVO2_PIN ??
+
 
 static inline void SmartDelay(int delayPeriod)
 {
