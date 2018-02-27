03b6101054803c513ae567a9992b1d7a04cb4e16
Amr Hossam <amrhossam902@gmail.com>
2018-02-28 00:08:05 +0200

    Push button logic added

--- Device code/atmega328P/atmega328P.ino
+++ Device code/atmega328P/atmega328P.ino
@@ -17,18 +17,105 @@ char *MonitorHealth()
   return "OK";
 }
 
-//TODO: Convert reading Button press event to ISRs instead of normal functions polling
-inline bool IsButtonPressedOnce()
+
+
+
+void PushButtonISR()
 {
-  // TODO: In case of using internal Pull-up resistor, change it to LOW instead
-  return (digitalRead(PUSH_BUTTON_PIN) == HIGH ? true : false);
+    // interrupts settings
+    sei();                // enable interrupts
+    EIMSK &= 0b11111110;   //disable int0 only
+
+    unsigned int pinState;
+    unsigned int pushCount = 0;
+    unsigned int relCount =0;
+   
+    while(1)
+    {
+		pinState = digitalRead(2);
+		if(pinState == 0)
+		{
+			pushCount++;
+			if(pushCount > PUSH_INTERVAL)
+            break;
+		}
+		else
+			pushCount = 0;
+	}
+
+	while(1)
+	{
+		pinState = digitalRead(2);
+		if(pinState == 1)
+		{
+			relCount++;
+			if(relCount > REL_INTERVAL)
+            break;
+		}
+		else
+			relCount = 0;
+	}
+
+   
+	relCount = 0;
+	pushCount = 0;
+	while(1)
+	{
+		pinState = digitalRead(2);
+		if(pinState == 1)
+		{
+			relCount++;
+			pushCount=0;
+
+			if(relCount > 33000)   // single push detected
+			{
+				Serial.println("Single push detected");
+			  SendRF_message("SOS1");
+        lockMax = true;
+        
+        // enable Interrupt on PIN 2 
+        EIFR &= 0b11111110;
+			  EIMSK |= 0b00000001;
+				return;
+			}
+		}
+		else
+		{
+			pushCount++;
+			relCount=0;
+
+			if(pushCount > PUSH_INTERVAL)  // double push detected
+			{ 
+        // wait for release
+				relCount = 0;
+				while(1)
+				{
+					pinState = digitalRead(2);
+					if(pinState == 1)
+					{
+						relCount++;
+						if(relCount > REL_INTERVAL)
+						break;
+					}
+					else
+						relCount=0;
+				} 
+       
+        Serial.println("Double push detected");
+        // TODO: Operate Servos!
+        SendRF_message("SOS2");
+        lockMax = true;
+        
+        // enable Interrupt on PIN 2
+				EIFR &= 0b11111110;
+				EIMSK |= 0b00000001;
+				return;
+			}    
+		}   
+   }   
 }
 
-inline bool IsButtonPressedTwice()
-{
-  // TODO: Check here if button pressed twice
-  return false;
-}
+
 
 void SendRF_message(char *msg)
 {
@@ -45,28 +132,21 @@ void SendRF_message(char *msg)
 void setup()
 {
   Serial.begin(115200);
-
-  pinMode(PUSH_BUTTON_PIN, INPUT);
-
+  
+  // push button initialization
+  pinMode(PUSH_BUTTON_PIN, INPUT_PULLUP);
+  sei();
+  attachInterrupt(digitalPinToInterrupt(2), PushButtonISR, FALLING);
+  
   MAX30100_init();
   RF_init();
 }
 
-bool lockMax = false; // HACK: Ignore Max30100 once successfully operating RF
+bool volatile lockMax = false; // HACK: Ignore Max30100 once successfully operating RF
 void loop()
 {
-  if (IsButtonPressedTwice())
-  {
-    // TODO: Operate Servos!
-    SendRF_message("SOS2");
-    lockMax = true;
-  }
-  else if (IsButtonPressedOnce())
-  {
-    SendRF_message("SOS1");
-    lockMax = true;
-  }
-  else if (!lockMax)
+ 
+  if (!lockMax)
   {
     char *state = MonitorHealth();
     if (state != "OK")
