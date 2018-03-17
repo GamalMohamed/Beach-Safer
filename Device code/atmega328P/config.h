20a0b0589b4955fa0fb7fb4af15cf484df205bd8
Gamal Mohamed <gamal.mohamed777@hotmail.com>
2018-03-18 00:58:59 +0200

    Updated some configs

--- Device code/atmega328P/config.h
+++ Device code/atmega328P/config.h
@@ -1,5 +1,5 @@
 // Physical device information for board and sensor
-#define DEVICE_ID "ESP12F"
+#define DEVICE_ID "777"
 
 #define MAX_RF_MSG_LENGTH 80
 #define RF_MSG_SENDING_PERIOD 30000
@@ -13,14 +13,14 @@
 #define PUSH_BUTTON_PIN 2
 #define RF_RX_PIN 4
 #define RF_TX_PIN 8
-#define RF_PTT_PIN 5 
+#define RF_PTT_PIN -1 
 #define SERVO_PIN 12
 
 #define NORMAL_STATE "OK"
 #define ALERT_STATE_L1 "SOS1"
 #define ALERT_STATE_L2 "SOS2"
 #define DROWNING_STATE "Drowning"
-#define DISTRESS_STATE
+#define DISTRESS_STATE "Distress"
 
 
 static inline void SmartDelay(int delayPeriod)
